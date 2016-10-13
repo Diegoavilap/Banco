ccad1dfb7affc3375f48fd25f3797bb821754338
eversame <leiser33@hotmail.com>
2016-10-13 16:18:20 -0500

    Hola hola camaradas

--- src/com/example/banco/Cajero2.java
+++ src/com/example/banco/Cajero2.java
@@ -1,7 +1,9 @@
 package com.example.banco;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class Cajero2 extends Activity {
@@ -9,5 +11,23 @@ public class Cajero2 extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.cajero2);
+	}
+	
+	public void Fun_consultasal(View v){
+		Intent i = new Intent();
+		i.setClass(Cajero2.this, Consulta.class);
+		startActivity(i);
+	}
+	
+	public void Fun_retiro(View v){
+		Intent i = new Intent();
+		i.setClass(Cajero2.this, Retiro.class);
+		startActivity(i);
+	}
+	
+	public void Fun_clave(View v){
+		Intent i = new Intent();
+		i.setClass(Cajero2.this, Clave.class);
+		startActivity(i);
 	}
 }
